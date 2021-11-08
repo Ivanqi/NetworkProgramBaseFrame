f59d0e8ee305d883102e9cc93518829b8bb793b7
Ivanqi <512350225@qq.com>
2021-11-08 21:47:20 +0800

    【修复】修复编译器指定问题

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ SOURCE_DIR=`pwd`
 BUILD_DIR=${BUILD_DIR:-./build}
 BUILD_TYPE=${BUILD_TYPE:-release}
 INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install-cpp17}
-CXX=${CXX:-g++}
+CXX=${CXX:-/usr/bin/g++}
 
 # CMAKE_BUILD_TYPE 设置build type
 # CMAKE_INSTALL_PREFIX 类似于configure脚本的 -prefix
@@ -15,10 +15,9 @@ CXX=${CXX:-g++}
 mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp17 \
     && cd $BUILD_DIR/$BUILD_TYPE-cpp17 \
     && cmake \
+        -DCMAKE_CXX_COMPILER=$CXX \
         -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
         -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         $SOURCE_DIR \
     && make $*
-
-
